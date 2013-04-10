eb747dfdd97ea0c0899c222c7f64d7a1f4f1fc08
stephan_ahlf <stephan_ahlf@ham-30-190.ham.s-f.com>
2013-04-10 10:54:29 +0200

    RefactorBaseClass

--- Refactor.py
+++ Refactor.py
@@ -11,35 +11,16 @@ from os.path import dirname, realpath
 REFACTOR_PLUGIN_FOLDER = dirname(realpath(__file__)) + "/"
 
 
-class RefactorCommand(sublime_plugin.TextCommand):
-    def run(self, edit):
-        self.save()
-        self.RefactorCommand(edit)
+class RefactorBaseClass(sublime_plugin.TextCommand):
+    currentCursorPosition = -1
 
     def save(self):
         self.view.run_command("save")
 
-    def RefactorCommand(self, edit):
-        scriptPath = REFACTOR_PLUGIN_FOLDER + "js/run.js"
-        settings = ' '.join([
-            "indent_size:\ 2",
-            "indent_char:\ ' '",
-            "max_char:\ 80",
-            "brace_style:\ collapse"
-        ])
-        tempFile = REFACTOR_PLUGIN_FOLDER + "tmp.txt.js"
-        jsonResultTempFile = REFACTOR_PLUGIN_FOLDER + "resultCodePositions.json"
-        #self.view.file_name()
-        cmd = ["node", scriptPath, tempFile, settings]
-        if len(self.view.sel()) != 1:
-            sublime.error_message("Multiple selection is not supported.")
+    def executeNodeJsShell(self, cmd):
         out = ""
         err = ""
-        refactoredText = ""
-        code = self.view.substr(self.view.sel()[0])
-        text_file = open(tempFile, "w")
-        text_file.write(code)
-        text_file.close()
+        result = ""
         if sublime.platform() == 'windows':
             p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE,  stderr=subprocess.PIPE)
             (out, err) = p.communicate()
@@ -47,32 +28,75 @@ class RefactorCommand(sublime_plugin.TextCommand):
             if err != '':
                 sublime.error_message(err)
             else:
-                refactoredText = out
+                result = out
         else:
             # fixme: fetch error messages
-            refactoredText = commands.getoutput('"'+'" "'.join(cmd)+'"')
+            result = commands.getoutput('"'+'" "'.join(cmd)+'"')
+        return result
 
-        if len(refactoredText) and err == "" > 0:
-            startPos = 0
-            for region in self.view.sel():
-                startPos = region.a
-                if region.b < startPos:
-                    startPos = region.b
-                self.view.replace(edit, region, refactoredText.decode('utf-8'))
-            #print startPos
-            sublime.set_timeout(self.save, 100)
+    def applyMultipleSelections(self, selections):
+        for region in selections:
+            r = sublime.Region(self.currentCursorPosition + region[0], self.currentCursorPosition + region[1])
+            self.view.sel().add(r)
+
+    def abortMultiselection(self):
+        if len(self.view.sel()) != 1:
+            sublime.error_message("Multiple selection is not supported.")
+            return True
+        else:
+            return False
+
+    def openJSONFile(self, filename):
+        json_file = open(filename)
+        data = json.load(json_file)
+        json_file.close()
+        return data
+
+    def writeTextFile(self, data, filename):
+        text_file = open(filename, "w")
+        text_file.write(data)
+        text_file.close()
+
+    def replaceCurrentTextSelection(self, edit, text):
+        startPos = 0
+        for region in self.view.sel():
+            startPos = region.a
+            if region.b < startPos:
+                startPos = region.b
+            self.view.replace(edit, region, text.decode('utf-8'))
+            self.currentCursorPosition = startPos
+        return startPos
+
+
+class RefactorCommand(RefactorBaseClass):
+    def run(self, edit):
+        self.RefactorCommand(edit)
+
+    def RefactorCommand(self, edit):
+        if self.abortMultiselection():
+            return
+
+        scriptPath = REFACTOR_PLUGIN_FOLDER + "js/run.js"
+        tempFile = REFACTOR_PLUGIN_FOLDER + "tmp.txt.js"
+        jsonResultTempFile = REFACTOR_PLUGIN_FOLDER + "resultCodePositions.json"
+        settings = ' '.join([
+            "indent_size:\ 2",
+            "indent_char:\ ' '",
+            "max_char:\ 80",
+            "brace_style:\ collapse"
+        ])
+
+        cmd = ["node", scriptPath, tempFile, settings]
+        code = self.view.substr(self.view.sel()[0])
+        self.writeTextFile(code, tempFile)
+        refactoredText = self.executeNodeJsShell(cmd)
+
+        if len(refactoredText):
+            self.replaceCurrentTextSelection(edit, refactoredText)
+
             self.view.sel().clear()
-
-            json_file = open(jsonResultTempFile)
-            data = json.load(json_file)
-            json_file.close()
-
-            for region in data:
-                #print region[0]
-                #print region[1]
-
-                r = sublime.Region(startPos+region[0], startPos+region[1])
-                self.view.sel().add(r)
+            selections = self.openJSONFile(jsonResultTempFile)
+            self.applyMultipleSelections(selections)
 
             os.remove(jsonResultTempFile)
         os.remove(tempFile)
